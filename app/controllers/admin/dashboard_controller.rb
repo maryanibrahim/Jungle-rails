9ebabf3681eb6fba358b94a1ba4ea922d661aa51
Maryan ibrahim <maryanibrahim301@yahoo.com>
2023-12-22 00:53:25 -0600

    Missing Admin Authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate_admin
+
   def show
+  end
+
+  private
+
+  def authenticate_admin
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 end
