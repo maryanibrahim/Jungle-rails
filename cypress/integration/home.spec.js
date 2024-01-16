6dc2d9a5ff9d4e43e3b8d5a5966344ada5c43737
Maryan ibrahim <maryanibrahim301@yahoo.com>
2024-01-16 08:11:10 -0600

    Create cypress test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+describe('home page', () => {
+  
+    it('can visit the home page', () => {
+      cy.visit('/');
+    });
+  
+    it("There are products on the page", () => {
+      cy.get(".products article").should("be.visible");
+    });
+  
+    it("There are 2 products on the page", () => {
+      cy.get(".products article").should("have.length", 2);
+    });
+  
+  })
